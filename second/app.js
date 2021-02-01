2a102470927860662112bf200270b55ab6cbf501
manas1316am <ad1316a@gmail.com>
2021-02-01 10:29:55 +0530

    second-commit

--- second/app.js
+++ second/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course on Time!'
+            courseGoal: 'Finish the course on Time!',
+            vueLink: 'https://vuejs.org'
         };
     }
 });
